5c221dafa0b5ae67f4a3313710bdea106e1d5c07
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2023-08-26 21:05:48 -0500

    Updated types to include the request and reply in the context

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,11 @@
 import { EntityManager } from "@mikro-orm/mongodb";
 import { OpenAI } from "langchain/llms/openai";
 import { ConversationChain } from "langchain/chains";
+import { FastifyReply, FastifyRequest } from "fastify";
 
 export type MyContext = {
+  request: FastifyRequest;
+  reply: FastifyReply;
   em: EntityManager;
   openai: OpenAI;
   chatgpt: ConversationChain;
