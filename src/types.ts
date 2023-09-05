fb1f76084ef43baf2909fd0feeaac6403bd7332e
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2023-09-04 20:28:07 -0500

    Added repetative input and response types to the types file

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
-// import { IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { EntityManager } from "@mikro-orm/mongodb";
 import { OpenAI } from "langchain/llms/openai";
 import { ConversationChain } from "langchain/chains";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { InputType, Field, ObjectType } from "type-graphql";
+
+/* Entities */
+import { FieldError } from "./entities/Errors/FieldError";
+import { User } from "./entities/User";
 
 export type MyContext = {
   request: FastifyRequest;
@@ -11,3 +15,23 @@ export type MyContext = {
   openai: OpenAI;
   chatgpt: ConversationChain;
 };
+
+/* --- Arguments (Args) Object Input Types --- */
+@InputType()
+export class UsernamePasswordInput {
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
+
+/* --- Response Object Types --- */
+@ObjectType()
+export class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
