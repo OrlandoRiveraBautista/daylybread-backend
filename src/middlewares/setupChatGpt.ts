56bc6aa3a3ce0b2bdcf70b49769b310611c81d29
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-01-13 19:41:37 -0600

    Updated the AI endpoint to request a device id as well as updated the set up of chat gpt to utilize the device id for memory (#9)

--- src/middlewares/setupChatGpt.ts
+++ src/middlewares/setupChatGpt.ts
@@ -1,32 +1,42 @@
-import { createMethodDecorator } from "type-graphql";
 import { MyContext } from "../types";
 import { BufferWindowMemory } from "langchain/memory";
 import { MikroORMChatMessageHistory } from "../misc/mikroormMessageHistory";
-import { generateObjectIdFromString } from "../utility";
-import ip from "ip";
 
 /**
  * Middleware function to setup the chatgpt prompt and memory
  */
-export const SetupChatGpt = () => {
-  return createMethodDecorator(
-    async ({ context }: { context: MyContext }, next) => {
-      // variables needed for function
-      const chain = context.chatgpt;
-      const memory = chain.memory as BufferWindowMemory;
-      // generate a new ObjectId based on the IP address
-      const chatId = generateObjectIdFromString(ip.address());
+// export const SetupChatGpt = () => {
+//   return createMethodDecorator(
+//     async ({ context }: { context: MyContext }, next) => {
+//       // variables needed for function
+//       const chain = context.chatgpt;
+//       const memory = chain.memory as BufferWindowMemory;
+//       // generate a new ObjectId based on the IP address
+//       const chatId = generateObjectIdFromString(ip.address());
 
-      console.log(ip.address());
+//       //   create a new message history and set it to chatHistory to be consumed by the model
+//       memory.chatHistory = new MikroORMChatMessageHistory({
+//         em: context.em,
+//         chatId: chatId,
+//         limit: memory.k,
+//       });
 
-      //   create a new message history and set it to chatHistory to be consumed by the model
-      memory.chatHistory = new MikroORMChatMessageHistory({
-        em: context.em,
-        chatId: chatId,
-        limit: memory.k,
-      });
+//       return next();
+//     }
+//   );
+// };
 
-      return next();
-    }
-  );
+export const setupChatGpt = async (context: MyContext, deviceId: string) => {
+  // variables needed for function
+  const chain = context.chatgpt;
+  const memory = chain.memory as BufferWindowMemory;
+  // generate a new ObjectId based on the IP address
+  // const chatId = generateObjectIdFromString(ip.address());
+
+  //   create a new message history and set it to chatHistory to be consumed by the model
+  memory.chatHistory = new MikroORMChatMessageHistory({
+    em: context.em,
+    chatId: deviceId,
+    limit: memory.k,
+  });
 };
