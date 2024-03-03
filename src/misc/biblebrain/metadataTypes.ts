20888dbd581bffa12c87540efcd22169f47f9e13
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-03-02 21:40:21 -0600

    Created metadata object type for response

---
+++ src/misc/biblebrain/metadataTypes.ts
@@ -0,0 +1,43 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+class BBPagination {
+  @Field()
+  total: number;
+
+  @Field({ nullable: true })
+  count?: number;
+
+  @Field()
+  per_page: number;
+
+  @Field()
+  current_page: number;
+
+  @Field()
+  total_pages: number;
+
+  @Field({ nullable: true })
+  links?: number;
+
+  @Field({ nullable: true })
+  last_page?: number;
+
+  @Field({ nullable: true })
+  next_page_url?: string;
+
+  @Field({ nullable: true })
+  prev_page_url?: string;
+
+  @Field({ nullable: true })
+  from?: number;
+
+  @Field({ nullable: true })
+  to?: number;
+}
+
+@ObjectType()
+export class BBMetadata {
+  @Field(() => BBPagination)
+  pagination: BBPagination;
+}
