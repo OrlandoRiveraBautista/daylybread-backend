43306716ffaa24bf4cf9c3ed951629c459f49b84
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-03-23 11:31:40 -0500

    Added bible response type to bible brain resolver types file

--- src/resolvers/Bible/BibleBrain/types.ts
+++ src/resolvers/Bible/BibleBrain/types.ts
@@ -1,11 +1,21 @@
 import { BBLanguage } from "../../../misc/biblebrain/languagesTypes";
 import { BBMetadata } from "../../../misc/biblebrain/metadataTypes";
+import { BBBible } from "../../../misc/biblebrain/bibleTypes";
 import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
 export class LanguageReponse {
   @Field(() => [BBLanguage])
   data: [BBLanguage];
+
+  @Field(() => BBMetadata)
+  meta: BBMetadata;
+}
+
+@ObjectType()
+export class BibleReponse {
+  @Field(() => [BBBible])
+  data: [BBBible];
 
   @Field(() => BBMetadata)
   meta: BBMetadata;
