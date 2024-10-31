ec959197d38bb28e79922741c45cd9828f40c05f
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2024-10-31 02:49:10 -0500

    Created a better prompt

--- src/resolvers/AI/GenerateImageResolver.ts
+++ src/resolvers/AI/GenerateImageResolver.ts
@@ -47,9 +47,11 @@ export class GenerateImageResolver {
     //   return error;
     // }
 
+    const defaultPrompt = `Create a detailed, realistic illustration set in ancient biblical times, reflecting the cultural and architectural elements of the period. The scene should capture the mood and significance of the events in the passage, focusing on the key figures and their emotions. Use lighting and colors to enhance the atmosphere, whether it’s a miraculous moment, a peaceful scene, or a challenging event. Aim for accuracy in period clothing, landscapes, and artifacts to bring the story to life. Requirements: Must not include text in the image. You do not include text in the image. You are an expert at ilustrating the bible. Text: ${prompt}`;
+
     try {
       const res = (await replicate.run("black-forest-labs/flux-schnell", {
-        input: { prompt },
+        input: { prompt: defaultPrompt },
       })) as string[]; // Pass the prompt for image generation
       console.log("Generated Image URL:", Object.keys(res)); // Output the generated image URL
 
