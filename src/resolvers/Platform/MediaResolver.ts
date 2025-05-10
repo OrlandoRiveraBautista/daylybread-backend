4d6545d548f07558c8a7572e6d7cc0f267349258
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2025-05-10 05:03:49 -0500

    Feat/nfc (#44)
    * Created an nfc config entity:
    
    * Created a media entity
    
    * Created an NFC config resolver
    
    * Added the nfc resolver to the server
    
    * Changed name field to title
    
    * Hella changes to the nfc config resolver
    
    * Created a media resolved
    
    * Added subdomain support

---
+++ src/resolvers/Platform/MediaResolver.ts
@@ -0,0 +1,192 @@
+import {
+  Resolver,
+  Query,
+  Arg,
+  Ctx,
+  Mutation,
+  InputType,
+  Field,
+  ObjectType,
+} from "type-graphql";
+import { Media, MediaPurpose } from "../../entities/Media";
+import { MyContext } from "../../types";
+import { ObjectId } from "@mikro-orm/mongodb";
+import { User } from "../../entities/User";
+import { FieldError } from "../../entities/Errors/FieldError";
+import { ValidateUser } from "../../middlewares/userAuth";
+
+@InputType()
+class MediaInput {
+  @Field(() => String)
+  url!: string;
+
+  @Field(() => String)
+  filename!: string;
+
+  @Field(() => String)
+  mimeType!: string;
+
+  @Field(() => Number)
+  size!: number;
+
+  @Field(() => MediaPurpose)
+  purpose!: MediaPurpose;
+
+  @Field(() => Boolean, { defaultValue: false })
+  isPublic?: boolean;
+
+  @Field(() => String, { nullable: true })
+  description?: string;
+}
+
+@ObjectType()
+class MediaResponse {
+  @Field(() => Media, { nullable: true })
+  results?: Media;
+
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+}
+
+@Resolver()
+export class MediaResolver {
+  @Query(() => Media)
+  async getMedia(@Arg("id") id: string, @Ctx() { em }: MyContext) {
+    return await em.findOne(Media, { _id: new ObjectId(id) });
+  }
+
+  @Query(() => [Media])
+  async getMediaByPurpose(
+    @Arg("purpose") purpose: MediaPurpose,
+    @Ctx() { em }: MyContext
+  ) {
+    return await em.find(Media, { purpose });
+  }
+
+  @ValidateUser()
+  @Mutation(() => MediaResponse)
+  async createMedia(
+    @Arg("options", () => MediaInput) options: MediaInput,
+    @Ctx() { em, request }: MyContext
+  ): Promise<MediaResponse> {
+    const req = request as any;
+
+    if (!req.userId) {
+      return {
+        errors: [
+          {
+            field: "User",
+            message: "User cannot be found. Please login first.",
+          },
+        ],
+      };
+    }
+
+    const user = await em.findOne(User, { _id: req.userId });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "User",
+            message: "No user found, try to log in.",
+          },
+        ],
+      };
+    }
+
+    const media = em.create(Media, {
+      ...options,
+      owner: user,
+      isPublic: options.isPublic || false,
+    });
+
+    try {
+      await em.persistAndFlush(media);
+    } catch (err) {
+      return {
+        errors: [
+          {
+            field: "Media",
+            message: "Failed to create media",
+          },
+        ],
+      };
+    }
+
+    return { results: media };
+  }
+
+  @ValidateUser()
+  @Mutation(() => MediaResponse)
+  async updateMedia(
+    @Arg("options", () => MediaInput) options: MediaInput,
+    @Arg("id", () => String) id: string,
+    @Ctx() { em }: MyContext
+  ): Promise<MediaResponse> {
+    const media = await em.findOne(Media, { _id: new ObjectId(id) });
+
+    if (!media) {
+      return {
+        errors: [
+          {
+            field: "Media",
+            message: "Media not found",
+          },
+        ],
+      };
+    }
+
+    em.assign(media, options);
+
+    try {
+      await em.persistAndFlush(media);
+    } catch (err) {
+      return {
+        errors: [
+          {
+            field: "Media",
+            message: "Failed to update media",
+          },
+        ],
+      };
+    }
+
+    return { results: media };
+  }
+
+  @ValidateUser()
+  @Mutation(() => MediaResponse)
+  async deleteMedia(
+    @Arg("id") id: string,
+    @Ctx() { em }: MyContext
+  ): Promise<MediaResponse> {
+    const media = await em.findOne(Media, { _id: new ObjectId(id) });
+
+    if (!media) {
+      return {
+        errors: [
+          {
+            field: "Media",
+            message: "Media not found",
+          },
+        ],
+      };
+    }
+
+    try {
+      await em.removeAndFlush(media);
+    } catch (err) {
+      return {
+        errors: [
+          {
+            field: "Media",
+            message: "Failed to delete media",
+          },
+        ],
+      };
+    }
+
+    return { results: media };
+  }
+}
